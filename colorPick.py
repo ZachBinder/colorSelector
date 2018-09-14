60bdf9f617b0b4218b2180ce35acc16407f6cb69
ZachBinder <38891887+ZachBinder@users.noreply.github.com>
2018-09-14 11:31:55 -0400

    initial commit
    initial commit

---
+++ colorPick.py
@@ -0,0 +1,48 @@
+"""
+Program: colorPick.py
+Author: Zachary Binder
+Date: 9-14-18
+"""
+
+from breezypythongui import EasyFrame
+import tkinter.colorchooser
+
+class ColorPicker(EasyFrame):
+	#Displaus the results of picking a color
+
+	def __init__(self):
+		#sets up the window and widgets
+		EasyFrame.__init__(self, title = "Color Chooser Demo")
+
+		#labels and output fields
+		self.addLabel('R', row = 0, column = 0)	
+		self.addLabel('G', row = 1, column = 0) 
+		self.addLabel('B', row = 2, column = 0)	
+		self.addLabel("Color", row = 3, column = 0)
+
+		self.r = self.addIntegerField(value = 0, row = 0, column = 1)
+		self.g = self.addIntegerField(value = 0, row = 1, column = 1)
+		self.b = self.addIntegerField(value = 0, row = 2, column = 1)
+		self.hex = self.addTextField(text = "#000000", row = 3, column = 1, width = 10)
+
+		#Canvas with an initial black background
+		self.canvas = self.addCanvas(row = 0, column = 2, rowspan = 4, width = 50, background = "#000000")
+
+		#command button 
+		self.addButton(text = "Choose Color", row = 4, column = 0, columnspan = 3, command = self.chooseColor)
+
+	def chooseColor(self):
+		#pops up a color chooser and outputs the results
+		colorTuple = tkinter.colorchooser.askcolor()
+		if not colorTuple[0]: 
+			return
+		((r,g,b), hexString) = colorTuple
+		self.r.setNumber(int(r))
+		self.g.setNumber(int(g))
+		self.b.setNumber(int(b))
+		self.hex.setText(hexString)
+		self.canvas["background"] = hexString
+
+def main():
+	ColorPicker().mainloop()
+main()
